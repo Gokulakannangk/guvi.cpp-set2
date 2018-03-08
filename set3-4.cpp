55e490e730e59d2129ae1b793dd0fe166894dcaa
Gokulakannangk <36393615+Gokulakannangk@users.noreply.github.com>
2018-03-08 17:55:41 +0530

    Create set3-4.cpp

---
+++ set3-4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	int a[200];
+	int n,i,j;
+	int temp;
+	
+	cout<<"enter the num to read: ";
+	cin>>n;
+
+	for(i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+
+	for(i=0;i<n;i++)
+	{		
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				temp  =a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+		cout<<a[i]<<"\t";
+	return 0;
+	
+}
