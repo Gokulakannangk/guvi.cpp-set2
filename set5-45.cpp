9ccd46b7690405c911f8fb68574586dbef2fbd9d
Gokulakannangk <36393615+Gokulakannangk@users.noreply.github.com>
2018-03-10 16:05:55 +0530

    Create set5-45.cpp

---
+++ set5-45.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout<<"ENTER THE NUMBER: ";
+    cin>>str;
+    cout<<"Number of digits :"<<str.length();
+    return 0;
+}
