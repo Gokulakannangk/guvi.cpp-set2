347aa46db62ccb37b9952a9f8be04d514028085a
Gokulakannangk <36393615+Gokulakannangk@users.noreply.github.com>
2018-03-10 15:56:57 +0530

    Create set5-44.cpp

---
+++ set5-44.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"ENTER THE NUM: ";
+    cin>>a;
+    if(a >= 1 && a <=10){
+        cout<<"yes";
+    }else{
+        cout<<"no";
+    }
+    return 0;
+}
