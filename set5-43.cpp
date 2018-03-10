207ec8b94ff2c923f357b1c71821ae4d053fd495
Gokulakannangk <36393615+Gokulakannangk@users.noreply.github.com>
2018-03-10 15:54:03 +0530

    Create set5-43.cpp

---
+++ set5-43.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str1,str2,str3;
+    cout<<"ENTER THE TWO STRING "<<endl;
+    cin>>str1>>str2;
+    str3 = str1+str2;
+    cout<<str3;
+    return 0;
+}
