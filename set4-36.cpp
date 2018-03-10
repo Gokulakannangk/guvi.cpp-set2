455c9b7a9c0a4c0c7c448136f8752e2dbe2290a1
Gokulakannangk <36393615+Gokulakannangk@users.noreply.github.com>
2018-03-10 14:26:01 +0530

    Update set4-36.cpp

--- set4-36.cpp
+++ set4-36.cpp
@@ -5,7 +5,7 @@ int main()
 {
     string str;
     int count = 0,i;
-    cout << "Enter a sentance: ";
+    cout << "ENTER THE SENTANCE : ";
     getline(cin, str);
     for(i = 0;i <= str.length()-1;i++){
         if(str[i] >= '0' && str[i] <= '9'){
