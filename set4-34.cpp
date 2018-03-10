679be06a5c0a27291fe302bd2b20d3c9222fcb8a
Gokulakannangk <36393615+Gokulakannangk@users.noreply.github.com>
2018-03-10 14:18:32 +0530

    Create set4-34.cpp

---
+++ set4-34.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string a
+    int count = 1,i;
+    cout << "ENTER THE SENTANCE : ";
+    getline(cin, a);
+    for(i = 0;i <= str.length()-1;i++){
+        if(a[i] == '.'){
+            count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
