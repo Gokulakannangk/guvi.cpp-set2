87486622a1a5432bc316258d1738a988b1a31737
Gokulakannangk <36393615+Gokulakannangk@users.noreply.github.com>
2018-03-12 17:22:18 +0530

    Create set5-49.cpp

---
+++ set5-49.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"ENTER THE TWO NUMBERS :";
+    cin>>a>>b;
+    a = a+b;
+    cout<<a;
+    return 0;
+}
+© 2018 GitHub, Inc.
