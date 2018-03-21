2d04ade6a236e54bfbda4caab3becfb345337910
Gokulakannangk <36393615+Gokulakannangk@users.noreply.github.com>
2018-03-21 14:13:32 +0530

    Create set7-64.cpp

---
+++ set7-64.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,m,sum;
+    cout<<"ENTER TWO NUMBER :";
+    cin>>n>>m;
+    sum=n+m;
+    if(sum%2==0)
+    cout<<"Even";
+    else
+    cout<<"odd";
+}
