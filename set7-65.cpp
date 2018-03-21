070493fa8256d1d23f03f9c27b465de9d92d16e6
Gokulakannangk <36393615+Gokulakannangk@users.noreply.github.com>
2018-03-21 14:16:29 +0530

    Create set7-65.cpp

---
+++ set7-65.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+int main()
+{
+string s;
+
+if(cout<<"ENTER THE STRING :")
+{
+    
+}
+if(getline(cin,s))
+{
+    
+}
+if (cout <<s) 
+{
+    
+}
+}
