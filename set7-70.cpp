0403b643c94d5c47bdff03150c9c8d3e13bef73c
Gokulakannangk <36393615+Gokulakannangk@users.noreply.github.com>
2018-03-27 11:05:12 +0530

    Create set7-70.cpp

---
+++ set7-70.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int n,res,i; 
+    cout<<"ENTER THE NUMBER ";
+    cin>>n;
+    for(i=1;i<n;i++){
+    res=pow(2,i);
+    if(n==2)
+    cout<<"Res:"<<pow(2,n);
+    else if(res>n)
+    {
+    cout<<"Res:"<<res;
+    break;
+    }
+}
+    return 0;
+}
